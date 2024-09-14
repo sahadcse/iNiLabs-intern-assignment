b0d5effd293e64b9c06c1730ec1c6115d86f6291
MD SAHADUZZAMAN <sahaduzzaman.cse@gmail.com>
2024-09-14 15:51:07 +0600

    Inheritance Implement Task One

--- inheritance.php
+++ inheritance.php
@@ -1,12 +1,45 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<h1>My first PHP page</h1>
-
 <?php
-echo "Hello World!";
-?>
 
-</body>
-</html>
+// Base class for geometric shapes
+class Shape {
+    public function area() {
+        return 0;
+    }
+}
+
+// Class for Circle
+class Circle extends Shape {
+    private $radius;
+
+    public function __construct($radius) {
+        $this->radius = $radius;
+    }
+
+    public function area() {
+        return pi() * pow($this->radius, 2);
+    }
+}
+
+// Class for Rectangle
+class Rectangle extends Shape {
+    private $width;
+    private $height;
+
+    public function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function area() {
+        return $this->width * $this->height;
+    }
+}
+
+// Example usage
+$circle = new Circle(5);
+echo "Circle Area: " . $circle->area() . "\n";
+
+$rectangle = new Rectangle(4, 6);
+echo "Rectangle Area: " . $rectangle->area() . "\n";
+
+?>
